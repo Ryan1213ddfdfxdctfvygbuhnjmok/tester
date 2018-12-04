07e3f3869c1ad5eeab9f13412fc2ec4cfb356470
Ryan1213ddfdfxdctfvygbuhnjmok <43233768+Ryan1213ddfdfxdctfvygbuhnjmok@users.noreply.github.com>
2018-12-04 13:37:04 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs')
+const moment = require('moment')
 const devs = ['439102535693762582'];
 var prefix = "-";
 const adminprefix = "-"
